a4965a5a8456118cf29372504ccff12be2b0b5cc
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-10-28 17:43:44 -0500

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Rock crushes Scissors
+// Scissors cuts Paper
+// Paper covers Rock
+// Rock crushes Lizard
+// Lizard poisons Spock
+// Spock smashes Scissors
+// Scissors decapitates Lizard
+// Lizard eats Paper
+// Paper disproves Spock
+// Spock vaporizes Rock
+
+//human vs human, human vs computer
+//best of three for winner
