b1fd04646f791ea492a5040b05ec8e58dff2056b
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-10-28 21:37:25 -0500

    compare gestures added

--- app.js
+++ app.js
@@ -19,17 +19,25 @@ class Game{
         this.playerOne = new Player("Harry");
         this.playerTwo = new Player("Ron");
 
-        
-
     }
 
     runGame(){
         this.displayRules();
 
-        this.playerOne.pickGesture();
-        let playerOneGesture = this.playerOne.pickGesture();
-        let playerTwoGesture = this.playerTwo.pickGesture();
 
+        this.playerOne.pickGesture();
+        this.playerTwo.pickGesture();
+        let result = this.playerOne.picked.compareGestures(this.playerTwo.picked);
+        if(result == 1){
+            this.playerOne.score ++;
+            console.log("Player one won");
+        }else if(result == -1){
+            this.playerTwo.score ++;
+            console.log("Player two won");
+        }else{
+            console.log("there is a tie");
+        }
+        //let winner = 
 
         // while(this.playerOne.score < 3 && this.playerTwo.score < 3){
         // }
@@ -39,6 +47,7 @@ class Game{
         console.log("Welcome to RPSLS");
         console.log("Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock")
     }
+    
 }
 
 
@@ -47,16 +56,17 @@ class Player{
         this.name = name;
         this.score = 0;
 
-        this.gestrues = [new Rock,"Paper","Scissors","Lizard","Spock"];
+        this.gestures = [new Rock, new Paper, new Scissors, new Lizard, new Spock];
+        this.picked = "";
     }
 
     pickGesture(){
         let chosenGesture = this.gestures[this.generateRandomNumber()];
-        return chosenGesture;
+        this.picked = chosenGesture;
     }
 
     generateRandomNumber(){
-        let randomNumber = Math.floor(Math.random() * 5) + 1;
+        let randomNumber = Math.floor(Math.random() * 5);
         return randomNumber;
     }
 }
@@ -64,21 +74,68 @@ class Player{
 class Gesture{
     constructor(){
         this.name = "";
+        this.win = [];
+        this.loose = [];
     }
 
     //child classes methods for winning and loosing
-
-
+    compareGestures(gestureTwo){
+        if(this.win.includes(gestureTwo.name)){
+            return 1;
+        }else if(this.loose.includes(gestureTwo.name)){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
 }
+
+
 
 class Rock extends Gesture{
     constructor(){
         super();
         this.name = "Rock";
-        //winning array
-        //loosing array
+        this.win = ["Scissors","Lizard"];
+        this.loose = ["Paper","Spock"];
+    }
+}
+
+class Scissors extends Gesture{
+    constructor(){
+        super();
+        this.name = "Scissors";
+        this.win = ["Paper","Lizard"];
+        this.loose = ["Rock","Spock"];
+    }
+}
+
+class Paper extends Gesture{
+    constructor(){
+        super();
+        this.name = "Paper";
+        this.win = ["Rock","Spock"];
+        this.loose = ["Scissors","Lizard"];
+    }
+}
+
+class Lizard extends Gesture{
+    constructor(){
+        super();
+        this.name = "Lizard";
+        this.win = ["Paper","Spock"];
+        this.loose = ["Scissors","Rock"];
+    }
+}
+
+class Spock extends Gesture{
+    constructor(){
+        super();
+        this.name = "Spock";
+        this.win = ["Rock","Scissors"];
+        this.loose = ["Paper","Lizard"];
     }
 }
 
 let game = new Game();
-console.log('game',game);
+game.runGame();
